8f52d4c33097363efb73c5d7eeed57ce578ae2ea
Naveenmavoor <Naveenkmavoor1@gmail.com>
2021-06-07 03:11:32 +0530

    optimized

---
+++ back-end-go/hospital-server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/emergency", http.HandlerFunc(emergency))
+
+	http.ListenAndServe(":8090", nil)
+}
+
+func emergency(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Analyzing Report...")
+	data, _ := ioutil.ReadAll(req.Body)
+
+	//print response  body
+	fmt.Printf("%s\n", data)
+	DispatchAmbulence()
+}
+
+func DispatchAmbulence() {
+	fmt.Println("Ambulence Dispatched")
+}
